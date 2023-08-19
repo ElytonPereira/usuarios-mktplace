2af49533cc02722cd4d470132cc3f5e14c2730c4
Elyton <fernandeselyton@gmail.com>
2023-08-18 22:10:37 -0300

    Implementado pré-requisitos para desenvolvimento da regra de negócio

--- src/main/java/br/com/senai/usuariosmktplace/core/InitApp.java
+++ src/main/java/br/com/senai/usuariosmktplace/core/InitApp.java
@@ -1,11 +1,16 @@
 package br.com.senai.usuariosmktplace.core;
 
+import br.com.senai.usuariosmktplace.core.service.UsuarioService;
+
 public class InitApp {
 
 	public static void main(String[] args) {
+		UsuarioService service = new UsuarioService();
 		
-		
-		
+		System.out.println(service.removerAcentoDo("José da Silvá"));
+		System.out.println(service.fracionar("José da Silvá Alburquerque dos anjos bragança"));
+		System.out.println(service.gerarLoginPor("José da Silvá Alburquerque dos anjos bragança"));
+		System.out.println(service.gerarHashDa("jose123456"));
 		
 	}
 
