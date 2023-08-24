b71a82ed17b3f1c3c2eeb57397c90d0bacfd4fac
Elyton <fernandeselyton@gmail.com>
2023-08-23 22:06:55 -0300

    inclusão de usuario no service

--- src/main/java/br/com/senai/usuariosmktplace/core/InitApp.java
+++ src/main/java/br/com/senai/usuariosmktplace/core/InitApp.java
@@ -7,11 +7,7 @@ public class InitApp {
 	public static void main(String[] args) {
 		UsuarioService service = new UsuarioService();
 		
-		System.out.println(service.removerAcentoDo("José da Silvá"));
-		System.out.println(service.fracionar("José da Silvá Alburquerque dos anjos bragança"));
-		System.out.println(service.gerarLoginPor("José da Silvá Alburquerque dos anjos bragança"));
-		System.out.println(service.gerarHashDa("jose123456"));
-		
+		service.criarPor("Elyton Fernandes Pereira","elyton1234");
 	}
 
 }
--- src/main/java/br/com/senai/usuariosmktplace/core/service/UsuarioService.java
+++ src/main/java/br/com/senai/usuariosmktplace/core/service/UsuarioService.java
@@ -7,6 +7,10 @@ import java.util.List;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.codec.digest.MessageDigestAlgorithms;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+
 import br.com.senai.usuariosmktplace.core.dao.DaoUsuario;
 import br.com.senai.usuariosmktplace.core.dao.FactoryDao;
 import br.com.senai.usuariosmktplace.core.domain.Usuario;
@@ -18,12 +22,24 @@ public class UsuarioService {
 	public UsuarioService() {
 		this.dao = FactoryDao.getInstance().getDaoUsuario();
 	}
+	
+	public Usuario criarPor(String nomeCompleto, String senha) {
+		this.validar(nomeCompleto, senha);
+		
+		String login = gerarLoginPor(nomeCompleto);
+		String senhaCriptografada = gerarHashDa(senha);
+		Usuario Novousuario = new Usuario(login, senhaCriptografada, nomeCompleto);
+		this.dao.inserir(Novousuario);
+		Usuario usuarioSalvo = dao.buscarPor(login);
+		return usuarioSalvo;
+		
+	}
 
-	public String removerAcentoDo(String nomeCompleto) {
+	private String removerAcentoDo(String nomeCompleto) {
 		return Normalizer.normalize(nomeCompleto, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
 	}
 	
-	public List<String> fracionar(String nomeCompleto){
+	private List<String> fracionar(String nomeCompleto){
 		List<String> nomeFracionado = new ArrayList<String>();
 		
 		if (nomeCompleto != null && !nomeCompleto.isBlank()) {			
@@ -42,7 +58,7 @@ public class UsuarioService {
 		
 	}
 	
-	public String gerarLoginPor(String nomeCompleto) {
+	private String gerarLoginPor(String nomeCompleto) {
 		nomeCompleto = removerAcentoDo(nomeCompleto);
 		List<String> partesDoNome = fracionar(nomeCompleto);
 		String loginGerado = null;
@@ -73,8 +89,37 @@ public class UsuarioService {
 		return loginGerado;
 	}
 	
-	public String gerarHashDa(String senha) {
-		return new DigestUtils(MessageDigestAlgorithms.MD5).digestAsHex(senha);
+	private String gerarHashDa(String senha) {
+		return new DigestUtils(MessageDigestAlgorithms.SHA3_256).digestAsHex(senha);
 		
 	}	
+	
+	@SuppressWarnings("deprecation")
+	private void validar(String senha) {
+		boolean isSenhaValida = !Strings.isNullOrEmpty(senha) && senha.length() >=6 && senha.length() <=15;
+		
+		Preconditions.checkArgument(isSenhaValida, "A senha é obrigatória e deve conter entre 6 e 15 caracteries");	
+		
+		boolean isContemLetra = CharMatcher.inRange('a', 'z').countIn(senha.toLowerCase()) >0;
+		boolean isContemNumero = CharMatcher.inRange('0', '9').countIn(senha) >0;
+		boolean isCaracterInvalido = !CharMatcher.javaLetterOrDigit().matchesAllOf(senha);
+				
+		Preconditions.checkArgument(!isCaracterInvalido && isContemNumero && isContemLetra, "A senha deve conter letras e numeros");
+		
+	}
+	
+	private void validar(String nomeCompleto, String senha) {
+		List<String> partesDoNome = fracionar(nomeCompleto);
+		
+		boolean isNomeCompleto = partesDoNome.size() >1;
+		boolean isNomeValido = !Strings.isNullOrEmpty(senha) &&  isNomeCompleto && nomeCompleto.length() >=5 && nomeCompleto.length() <120;
+		
+		Preconditions.checkArgument(isNomeValido, "O nome é obrigatório e deve conter entre 5 e 120 caracteries e conter sobrenome também");
+		this.validar(senha);
+		
+	}
+	
+	
+	
+	
 }
