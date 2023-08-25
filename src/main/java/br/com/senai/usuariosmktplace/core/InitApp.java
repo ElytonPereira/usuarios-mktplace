494b5fabeba528eae3831f160a67ca14c31bcc63
Elyton <fernandeselyton@gmail.com>
2023-08-24 22:19:00 -0300

    Inicio trabalho lombok

--- src/main/java/br/com/senai/usuariosmktplace/core/InitApp.java
+++ src/main/java/br/com/senai/usuariosmktplace/core/InitApp.java
@@ -1,10 +1,16 @@
 package br.com.senai.usuariosmktplace.core;
 
+import br.com.senai.usuariosmktplace.core.dao.ManagerDb;
+import br.com.senai.usuariosmktplace.core.domain.Usuario;
+
 public class InitApp {
 
 	public static void main(String[] args) {
+		ManagerDb.getInstance().getConexao();
+		System.out.println("Conectou");
 		
-		
+		Usuario usuario = new Usuario("Elyton teste", "Jose Elyton", "jose1234");
+		usuario.getLogin();
 		
 		
 	}
